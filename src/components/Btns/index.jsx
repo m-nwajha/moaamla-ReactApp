9fa9398cbb37de8e5cccb0404bdff86db1b44aff
M-NAWJHA <m.nawjha@gmail.com>
2025-04-10 11:19:21 +0200

    Create Btns Components {BtnGreen, BtnBlue}.

---
+++ src/components/Btns/index.jsx
@@ -0,0 +1,57 @@
+// Import Style css
+import './style.css';
+
+/****
+ * Btn Green Component *
+ *****/
+export const BtnGreen = ({
+  // Children(Label) Prop.
+  children,
+
+  // On Click Prop.
+  onClick,
+
+  // Class Name Prop.
+  className = '',
+
+  // If passing 'reset' Prop Btn Type Reset, else Btn Type Default Submit.
+  reset
+}) => {
+  return (
+    <button
+      className={`btn_green ${className}`}
+      onClick={onClick}
+      type={reset ? 'reset' : 'submit'}>
+      {children}
+    </button>
+  );
+};
+
+
+
+
+/****
+ * Btn Blue Component *
+ *****/
+export const BtnBlue = ({
+  // Children(Label) Prop.
+  children,
+
+  // On Click Prop.
+  onClick,
+
+  // Class Name Prop.
+  className = '',
+
+  // If passing 'reset' Prop Btn Type Reset, else Btn Type Default Submit.
+  reset
+}) => {
+  return (
+    <button
+      className={`btn_blue ${className}`}
+      onClick={onClick}
+      type={reset ? 'reset' : 'submit'}>
+      {children}
+    </button>
+  );
+};
